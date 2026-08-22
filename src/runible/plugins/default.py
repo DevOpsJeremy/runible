d69754a74f3bf4242c4ea7a826aabe88f2e7984c
DevOps Jeremy <devopsjeremy@gmail.com>
2026-08-22 12:41:34 -0700

    fix default interface doc

--- src/runible/plugins/default.py
+++ src/runible/plugins/default.py
@@ -2,5 +2,7 @@ from ..interface import Interface
 
 
 class DefaultInterface(Interface):
+    """Default interface. This disables all output."""
+
     def __init__(self):
         super().__init__(quiet=False)
